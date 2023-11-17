384ec05bd669b27112ef770877d6a140b2df72d1
Marcelo Inocente <menyeath@hotmail.com>
2023-11-17 12:29:03 -0500

    Use trained model

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-happy_tt = HappyTextToText("T5", "vennify/t5-base-grammar-correction")
+model = HappyTextToText("T5", model_name="train/model/")
 args = TTSettings(num_beams=5, min_length=1)
 
 @app.route("/api/correct", methods=['POST'])
 def correct():
     user_text = request.json['text']
-    result = happy_tt.generate_text(f"grammar: {user_text}", args=args)
+    result = model.generate_text(f"grammar: {user_text}", args=args)
     text = result.text.strip('.')
 
     return {
